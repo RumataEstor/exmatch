72dcec2f7d1c1695bc3d908ad335030e919336c2
Dmitry Belyaev <be.dmitry@gmail.com>
2022-03-10 00:06:43 +1100

    Version 0.2.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exmatch.MixProject do
     do: [
       app: :exmatch,
       description: description(),
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.13",
       elixirc_options: [warnings_as_errors: true],
       elixirc_paths: ["lib"] ++ if(Mix.env() == :test, do: ["test/support"], else: []),
