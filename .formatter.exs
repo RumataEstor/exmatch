4525193933973967cb1db33bfa363710c7c78f39
Dmitry Belyaev <be.dmitry@gmail.com>
2022-11-01 23:08:32 +1100

    Allow exmatch equality expressions

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,8 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  export: [
+    locals_without_parens: [exmatch: 2, exmatch: 1]
+  ],
+  locals_without_parens: [exmatch: 2, exmatch: 1]
 ]
--- lib/exmatch.ex
+++ lib/exmatch.ex
@@ -15,14 +15,18 @@ defmodule ExMatch do
 
   alias ExMatch.ParseContext
 
+  defmacro match(expr) do
+    gen_match(expr, quote(do: ExMatch.default_options()))
+  end
+
   @doc """
   Raises if the values don't match and displays what exactly was different.
 
   iex> ExMatch.match([1, a, 3], [1, 2, 3])
   iex> 2 = a
   """
-  defmacro match(left, right) do
-    gen_match(left, right, parse_options([]))
+  defmacro match(arg1, arg2) do
+    gen_match(arg1, arg2)
   end
 
   defmacro match(left, right, opts) do
@@ -37,12 +41,41 @@ defmodule ExMatch do
     ExMatch.Options.parse(item)
   end
 
+  def default_options() do
+    options([])
+  end
+
+  defp gen_match({:==, _, [left, right]}, opts) do
+    gen_match(left, right, opts)
+  end
+
+  defp gen_match({:=, _, [left, right]}, opts) do
+    gen_match(left, right, opts)
+  end
+
+  defp gen_match(arg1, arg2) do
+    right =
+      quote do
+        case unquote(arg2) do
+          %ExMatch.Options{} ->
+            raise "The pattern #{unquote(Macro.to_string(arg1))} is not yet supported"
+
+          value ->
+            value
+        end
+      end
+
+    gen_match(arg1, right, quote(do: ExMatch.default_options()))
+  end
+
   def gen_match(left, right, opts_expr) do
     opts_var = Macro.var(:opts, __MODULE__)
     parse_context = %ParseContext{opts: opts_var}
     {bindings, left} = ParseContext.parse(left, parse_context)
 
     quote location: :keep do
+      right = unquote(right)
+
       unquote(opts_var) =
         case unquote(opts_expr) do
           %ExMatch.Options{opts: opts} ->
@@ -53,7 +86,7 @@ defmodule ExMatch do
         end
 
       unquote(bindings) =
-        case ExMatch.Match.diff(unquote(left), unquote(right), unquote(opts_var)) do
+        case ExMatch.Match.diff(unquote(left), right, unquote(opts_var)) do
           {diff_left, diff_right} = diff ->
             raise unquote(@assertion_error),
               left: diff_left,
