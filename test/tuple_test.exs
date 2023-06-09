de21c488a184b24175c51eff879f09840beffb17
Dmitry Belyaev <be.dmitry@gmail.com>
2023-06-09 14:30:58 +1000

    Move List and Tuple tests into separate modules

---
+++ test/tuple_test.exs
@@ -0,0 +1,65 @@
+defmodule ExMatchTest.Tuple do
+  use ExMatchTest.TestCase
+  require ExMatch
+
+  test "match" do
+    ExMatch.match({}, {})
+    ExMatch.match({1}, {1})
+    ExMatch.match({1, 2}, {1, 2})
+    ExMatch.match({_, 2}, {1, 2})
+    ExMatch.match({1, 2, 3}, {1, 2, 3})
+    ExMatch.match({1, 2, 3, 4}, {1, 2, 3, 4})
+    ExMatch.match({a, 2, 3, b}, {1, 2, 3, 4})
+    assert {a, b} == {1, 4}
+  end
+
+  test "diff in tail" do
+    match_fails(
+      ExMatch.match({1, 2, 4}, {1, 2, 3}),
+      """
+      left:  {..2.., 4}
+      right: {..2.., 3}
+      """
+    )
+  end
+
+  test "nothing matches" do
+    match_fails(
+      ExMatch.match({1, 2, 4}, {2, 4}),
+      """
+      left:  {1, 2, 4}
+      right: {2, 4}
+      """
+    )
+  end
+
+  test "diff in the middle" do
+    match_fails(
+      ExMatch.match({1, {2, 3}, 4}, {1, 2, 4}),
+      """
+      left:  {.., {2, 3}, ..}
+      right: {.., 2, ..}
+      """
+    )
+  end
+
+  test "diff with ignored value" do
+    match_fails(
+      ExMatch.match({_, 2}, {1, 2, 3, 4}),
+      """
+      left:  {..2..}
+      right: {..2.., 3, 4}
+      """
+    )
+  end
+
+  test "one matches fully" do
+    match_fails(
+      ExMatch.match({1, 2}, {1, 2, 3, 4}),
+      """
+      left:  {..2..}
+      right: {..2.., 3, 4}
+      """
+    )
+  end
+end
