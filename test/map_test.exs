184dcdcdcd64debc6e162d7c10a138e5c54908cc
Dmitry Belyaev <be.dmitry@gmail.com>
2023-06-13 11:11:37 +1000

    Expand map tests

--- test/map_test.exs
+++ test/map_test.exs
@@ -15,6 +15,9 @@ defmodule ExMatchTest.Map do
     ExMatch.match(%{..., b: ExMatchTest.Dummy.id(two), c: b, d: %{..., f: a}}, @map)
     assert a == 12
     assert b == 3
+
+    ref = make_ref()
+    ExMatch.match(%{"key" => 1, {1, 2} => 2, ref => 3}, %{"key" => 1, {1, 2} => 2, ref => 3})
   end
 
   test "diff skips a matching field" do
@@ -39,8 +42,18 @@ defmodule ExMatchTest.Map do
     )
   end
 
+  test "diff skipped fields" do
+    match_fails(
+      ExMatch.match(%{..., a: 1, c: 3, d: %{e: 11, f: 12}}, 1),
+      """
+      left:  %{..., a: 1, c: 3, d: %{e: 11, f: 12}}
+      right: 1
+      """
+    )
+  end
+
   test "diff whole map behind a variable" do
-    map = @map
+    map = Map.put(@map, "key", 10)
 
     match_fails(
       ExMatch.match(^map, 1),
@@ -96,6 +109,30 @@ defmodule ExMatchTest.Map do
       left:  %{d: ExMatchTest.Dummy.id(eleven + two) = 13, x: _, y: 6}
       right: %{d: %{e: 11, f: 12}, b: 2}
       """
+    )
+  end
+
+  test "diff shows non-atom keys" do
+    ref = make_ref()
+
+    match_fails(
+    ExMatch.match(%{"key" => 1, {1, 2} => 2, ref => 3}, %{"key" => 8, {1, 2} => 2, ref => 4}),
+    """
+    left:  %{#{inspect(ref)} => 3, "key" => 1}
+    right: %{#{inspect(ref)} => 4, "key" => 8}
+    """
+    )
+  end
+
+  test "diff shows non-atom keys in partial pattern" do
+    ref = make_ref()
+
+    match_fails(
+    ExMatch.match(%{..., "key" => 1, ref => 3}, %{"key" => 8, {1, 2} => 2, ref => 4}),
+    """
+    left:  %{#{inspect(ref)} => 3, "key" => 1}
+    right: %{#{inspect(ref)} => 4, "key" => 8}
+    """
     )
   end
 end
